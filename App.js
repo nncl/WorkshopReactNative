56152a01fcff7d5cb28a21fda8627f82a39d4d7c
= <nncl@live.com>
2018-01-03 19:23:32 -0200

    Adding repo from Github API

--- App.js
+++ App.js
@@ -15,24 +15,56 @@ import {
 } from 'react-native';
 
 import Repo from './components/Repo';
+import NewRepoModal from './components/NewRepoModal';
 
 export default class App extends Component {
 
+    /*
+     repos : [{
+     id: 1,
+     thumbnail: 'https://avatars0.githubusercontent.com/u/189580',
+     title: 'Multer',
+     author: 'expressjs'
+     },
+     {
+     id: 2,
+     thumbnail: 'https://avatars2.githubusercontent.com/u/2151889',
+     title: 'Kue',
+     author: 'Automattic'
+     }]
+     */
+
     state = {
-        repos: [
-            {
-                id: 1,
-                thumbnail: 'https://avatars0.githubusercontent.com/u/189580',
-                title: 'Multer',
-                author: 'expressjs'
-            },
-            {
-                id: 2,
-                thumbnail: 'https://avatars2.githubusercontent.com/u/2151889',
-                title: 'Kue',
-                author: 'Automattic'
-            }
-        ]
+        modalVisible: false,
+        repos: []
+    };
+
+    /**
+     * @description
+     * Add repo.
+     *
+     * @param {String} newRepoText
+     * @private
+     */
+
+    _addRepo = async (newRepoText) => {
+        const repoCall = await fetch(`http://api.github.com/repos/${newRepoText}`)
+            , response = await repoCall.json();
+
+        const newRepo = {
+            id: response.id
+            , thumbnail: response.owner.avatar_url
+            , title: response.name
+            , author: response.owner.login
+        };
+
+        this.setState({
+            modalVisible: false
+            , repos: [
+                ...this.state.repos // Add all existent repos
+                , newRepo
+            ]
+        });
     };
 
     render() {
@@ -43,8 +75,7 @@ export default class App extends Component {
                         React Native Minicourse!
                     </Text>
 
-                    <TouchableOpacity onPress={() => {
-                    }}>
+                    <TouchableOpacity onPress={() => this.setState({modalVisible: true})}>
                         <Text style={styles.headerButton}>+</Text>
                     </TouchableOpacity>
                 </View>
@@ -52,6 +83,10 @@ export default class App extends Component {
                 <ScrollView contentContainerStyle={styles.repoList}>
                     { this.state.repos.map(repo => <Repo key={repo.id} data={repo}/>)}
                 </ScrollView>
+
+                <NewRepoModal onCancel={() => this.setState({modalVisible: false})}
+                              onAdd={this._addRepo}
+                              visible={this.state.modalVisible}/>
             </View>
         );
     }
---
+++ components/NewRepoModal.js
@@ -0,0 +1,111 @@
+/**
+ * @description
+ * New Github repo modal component
+ */
+
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    Modal,
+    TouchableOpacity
+} from 'react-native';
+
+export default class NewRepoModal extends Component {
+    state = {
+        newRepoText: ''
+    };
+
+    render() {
+        return (
+            <Modal animationType="fade"
+                   transparent={true}
+                   visible={this.props.visible}>
+
+                <View style={styles.modalContainer}>
+                    <View style={styles.boxContainer}>
+                        <Text style={styles.boxTitle}>Add Repo</Text>
+                        <TextInput autoFocus
+                                   autocapitalize="none"
+                                   style={styles.boxInput}
+                                   underlineColorAndroid="rgba(0,0,0,0)"
+                                   placeholder="Org/Repo"
+                                   value={this.state.newRepoText}
+                                   onChangeText={newRepoText => this.setState({newRepoText})}/>
+
+                        <View style={styles.buttonContainer}>
+                            <TouchableOpacity
+                                style={[styles.button, styles.cancelButton]}
+                                onPress={this.props.onCancel}>
+                                <Text style={styles.buttonText}>Cancel</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={[styles.button, styles.submitButton]}
+                                onPress={() => this.props.onAdd(this.state.newRepoText)}>
+                                <Text style={styles.buttonText}>Add</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+
+            </Modal>
+        )
+            ;
+    }
+}
+
+const styles = {
+    boxContainer: {
+        padding: 20,
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        alignItems: 'center',
+        width: 280
+    }
+    , modalContainer: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+    , boxTitle: {
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+    , boxInput: {
+        alignSelf: 'stretch',
+        marginTop: 10,
+        paddingVertical: 0,
+        paddingHorizontal: 20,
+        borderWidth: 1,
+        borderColor: '#DDD',
+        height: 40,
+        borderRadius: 3
+    }
+    , buttonContainer: {
+        marginTop: 10,
+        height: 40,
+        flexDirection: 'row'
+    }
+    , button: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 3
+    }
+    , cancelButton: {
+        backgroundColor: '#E25F5F',
+        marginRight: 5
+    }
+    , submitButton: {
+        backgroundColor: '#70BD85',
+        marginLeft: 5
+    }
+    , buttonText: {
+        fontWeight: 'bold',
+        color: '#FFF',
+        fontSize: 12
+    }
+};
